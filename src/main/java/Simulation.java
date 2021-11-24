ef644cb17d49275aad734949e1a2e07a52ed78bf
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-23 23:07:04 -0500

    i give up for now :(

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,33 @@
 public class Simulation {
+        private Bins bins;
+        private Dice dice;
+        private Integer numThrows;
 
+        public Simulation(Integer numDice, Integer numThrows) {
+            this.bins = new Bins(numDice,6*numDice);
+            this.dice = new Dice(numDice);
+            this.numThrows=numThrows;
+        }
 
+        public void runSimulation(){
+            for(int i=0;i<this.numThrows;i++){
+                int thisToss=0;
+                thisToss = this.dice.toss();
+                this.bins.incrementBin(thisToss);
+            }
+        }
 
+        public void printResult(){
+            System.out.println("***");
+            System.out.println("Simulation of "+dice.numDice+" dice tossed for "+numThrows+" times.");
+            System.out.println("***\n");
+            for(int i=dice.numDice;i<=6*dice.numDice;i++){
+                Integer thisCount = bins.getBin(i);
+                Double thisPercent = (double)thisCount/(double)numThrows;
+                Integer thisStars = (int)(thisPercent*100);
+                StringBuilder builder=new StringBuilder("");
+                for(int j=0;j<thisStars;j++)builder.append("*");
+                System.out.println(String.format("%2d:%9d: %1.2f ",i,thisCount,thisPercent)+builder.toString());
+            }
+        }
 }
